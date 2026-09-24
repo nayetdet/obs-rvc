0817c3d7ddbf2fb4a911ba2d7cac9f6c17820dee
nayetdet <jpmoreiracampos21@gmail.com>
2026-09-24 16:12:53 +0000

    fix: correct extern "C" block formatting in worker process header

--- plugin/src/rvc/worker/worker-process.h
+++ plugin/src/rvc/worker/worker-process.h
@@ -2,7 +2,15 @@
 
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct rvc_worker_process rvc_worker_process_t;
 
 bool rvc_worker_start(rvc_worker_process_t **process);
 void rvc_worker_stop(rvc_worker_process_t *process);
+
+#ifdef __cplusplus
+}
+#endif
