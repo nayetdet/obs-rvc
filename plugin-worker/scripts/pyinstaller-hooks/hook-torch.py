c67bf2ac2cebb809554a0d8dd9702a7e27b1afce
nayetdet <jpmoreiracampos21@gmail.com>
2026-09-25 14:16:13 +0000

    feat: enhance PyInstaller hook for Torch by adding dynamic library management

--- plugin-worker/scripts/pyinstaller-hooks/hook-torch.py
+++ plugin-worker/scripts/pyinstaller-hooks/hook-torch.py
@@ -1,4 +1,6 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
+from pathlib import Path
+
+from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs, get_package_paths
 
 
 datas = collect_data_files(
@@ -11,8 +13,12 @@ datas = collect_data_files(
         "**/*.cpp",
         "**/*.pyi",
         "**/*.cmake",
-        "**/bin/*",
         "**/test/**",
     ],
 )
+
 binaries = collect_dynamic_libs("torch")
+torch_package = Path(get_package_paths("torch")[1])
+torch_shm_manager = torch_package / "bin" / "torch_shm_manager"
+if torch_shm_manager.is_file():
+    binaries.append((str(torch_shm_manager), "torch/bin"))
